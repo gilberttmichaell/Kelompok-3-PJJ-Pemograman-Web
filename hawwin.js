e0ab129c51d2f979e20236b61367d03546203855
hawwinrizkih <hawwinrizki@gmail.com>
2026-06-15 17:33:35 +0700

    hawwin upload

---
+++ hawwin.js
@@ -0,0 +1,12 @@
+const nama = "Hawwin"; //type data String
+const umur = 30; //type data number
+const isMarried = true; //boolean 
+// const dateAt; //undifiend
+
+console.log(typeof nama, typeof umur, isMarried);// menampilkan type data
+
+//kumpulan type data JavaScript
+//template literals string bisa difungsikan ke pemprosesan data
+const greeting = `Hello, my name ${nama}. Umur Saya ${2026 - umur}`;
+
+console.log(greeting);
