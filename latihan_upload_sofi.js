700a73a896cdb16af35e5f85f57bcf0d8a91f0bd
SOFIA NOVIANI <817931578@intranet.pajak.go.id>
2026-06-15 14:20:54 +0700

    Latihan Upload file Senin, 15 Juni 2026

---
+++ latihan_upload_sofi.js
@@ -0,0 +1,22 @@
+const nama = "Arip";
+const umur = 29;
+const isMarried = true;
+// const dateAt; // undifiend
+
+console.log(typeof nama, typeof umur, isMarried)
+console.log(nama,umur, isMarried)
+
+//kumpulan type data JavaScript
+
+
+const greeting = `Hello, my name is ${nama}. Umur saya ${umur}`;
+
+console.log (greeting)
+
+
+const age = 23;
+
+if (age > 21) {console.log("Dewasa");
+    
+}
+
